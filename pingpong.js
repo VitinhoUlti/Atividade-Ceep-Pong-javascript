70f402b92eca066d42833cab021094710db5ecea
Vitor Miguel Santos Redondo <140042669+VitinhoUlti@users.noreply.github.com>
2024-07-25 17:20:26 -0300

    velocidade

--- pingpong.js
+++ pingpong.js
@@ -2,6 +2,9 @@ let xbolinha = 300;
 let ybolinha = 200;
 let tambolinha = 25;
 
+let xVelbolinha = 6;
+let yVelbolinha = 6;
+
 function setup(){
     createCanvas(600, 400);
 }
@@ -9,8 +12,14 @@ function setup(){
 function draw(){
     background(128, 128, 0);
     criaBolinha(xbolinha, ybolinha, tambolinha);
+    moveBolinha();
 }
 
 function criaBolinha(xbolinha, ybolinha, tambolinha){
     circle(xbolinha, ybolinha, tambolinha)
+}
+
+function moveBolinha(){
+    xBolinha += xVelbolinha;
+    yBolinha += yVelbolinha;
 }
