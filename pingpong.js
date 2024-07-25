15234e32fea013dbc36dbab7654419c4effdfdfa
Vitor Miguel Santos Redondo <140042669+VitinhoUlti@users.noreply.github.com>
2024-07-25 16:56:41 -0300

    começo

---
+++ pingpong.js
@@ -0,0 +1,16 @@
+let xbolinha = 300;
+let ybolinha = 300;
+let tambolinha = 250;
+
+function setup(){
+    createCanvas(600, 400);
+}
+
+function draw(){
+    backgroud(128, 128, 0);
+    criaBolinha(xbolinha, ybolinha, tambolinha);
+}
+
+function criaBolinha(xbolinha, ybolinha, tambolinha){
+    circle(xbolinha, ybolinha, tambolinha)
+}
