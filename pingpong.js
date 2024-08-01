90af2130abb212522b9aefb20142fba52cd05dcb
Vitor Miguel Santos Redondo <140042669+VitinhoUlti@users.noreply.github.com>
2024-08-01 15:54:15 -0300

    teste1

--- pingpong.js
+++ pingpong.js
@@ -22,9 +22,6 @@ function draw(){
     criaBolinha(xbolinha, ybolinha, tambolinha);
     moveBolinha();
     Borda();
-    criarRaquete(xRaquete, yRaquete, larguraRaquete, alturaRaquete);
-    movimentaRaquete();
-    colideRaquete();
 }
 
 function criaBolinha(xbolinha, ybolinha, tambolinha){
@@ -44,24 +41,4 @@ function Borda(){
     if(ybolinha > width || ybolinha < 0){
         yVelbolinha *= -1;
     }
-}
-
-function criarRaquete(xRaquete, yRaquete, larguraRaquete, alturaRaquete){
-    fill("blue");
-    rect(xRaquete, yRaquete, larguraRaquete, alturaRaquete);
-}
-
-function movimentaRaquete() {
-    if(keyIsDown(UP_ARROW)){
-        yRaquete -= 10;
-    }
-    if(keyIsDown(DOWN_ARROW)){
-        yRaquete += 10;
-    }
-}
-
-function colideRaquete(){
-    if(xBolinha - raioBolinha < xRaquete + larguraRaquete && ybolinha - raioBolinha < yRaquete + alturaRaquete && yBolinha + raioBolinha > yRaquete){
-        xvelocidade *= -1;
-    } 
 }
