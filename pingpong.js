f26d6bdafbc0bf73581ceb7b413a1e9b20edad80
Vitor Miguel Santos Redondo <140042669+VitinhoUlti@users.noreply.github.com>
2024-08-01 14:14:36 -0300

    ping pong modo hard

--- pingpong.js
+++ pingpong.js
@@ -2,35 +2,39 @@ let xbolinha = 300;
 let ybolinha = 200;
 let tambolinha = 25;
 
-let xVelbolinha = 600;
-let yVelbolinha = 600;
+let xVelbolinha = 6;
+let yVelbolinha = 6;
 
-function setup(){
-    createCanvas(2000, 2000);
-}
+let tamanho = 2000;
 
-function draw(){
-    background(128, 128, 0);
-    criaBolinha(xbolinha, ybolinha, tambolinha);
-    moveBolinha();
-    Borda();
-}
-
-function criaBolinha(xbolinha, ybolinha, tambolinha){
-    circle(xbolinha, ybolinha, tambolinha)
-}
-
-function moveBolinha(){
-    xbolinha += xVelbolinha;
-    ybolinha += yVelbolinha;
-}
-
-function Borda(){
-    if(xbolinha > 2000 || xbolinha < 0){
-        xVelbolinha *= -1;
+function Criar(){
+    function setup(){
+        createCanvas(tamanho, tamanho);
     }
-
-    if(ybolinha > 2000 || ybolinha < 0){
-        yVelbolinha *= -1;
+    
+    function draw(){
+        background(128, 128, 0);
+        criaBolinha(xbolinha, ybolinha, tambolinha);
+        moveBolinha();
+        Borda();
+    }
+    
+    function criaBolinha(xbolinha, ybolinha, tambolinha){
+        circle(xbolinha, ybolinha, tambolinha)
+    }
+    
+    function moveBolinha(){
+        xbolinha += xVelbolinha;
+        ybolinha += yVelbolinha;
+    }
+    
+    function Borda(){
+        if(xbolinha > width || xbolinha < 0){
+            xVelbolinha *= -1;
+        }
+    
+        if(ybolinha > width || ybolinha < 0){
+            yVelbolinha *= -1;
+        }
     }
 }
